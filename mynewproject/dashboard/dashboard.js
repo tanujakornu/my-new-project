2fd6112a02eccdb9dde00b943cff8e95cc90f853
tanujakornu <tkornu@gitam.in>
2025-03-19 06:21:29 +0530

    Update dashboard.js

--- mynewproject/dashboard/dashboard.js
+++ mynewproject/dashboard/dashboard.js
@@ -1,16 +1,4 @@
-function uploadFile() {
-    alert("Upload functionality coming soon!");
-}
-
-function viewFiles() {
-    alert("Viewing files functionality coming soon!");
-}
-
-function downloadFiles() {
-    alert("Download functionality coming soon!");
-}
-
-document.getElementById("logoutBtn").addEventListener("click", function() {
-    alert("Logging out...");
-    window.location.href = "../login/login.html";
+document.getElementById("logout").addEventListener("click", function() {
+    alert("Logged out!");
+    window.location.href = "../login/index.html"; // Redirect to login page
 });
